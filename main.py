ffb29bd8676dfdd445c4033392050ad7bf2139ef
TsePinDF <99466319+TsePinDF@users.noreply.github.com>
2023-12-17 20:13:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 df = pd.read_csv("assets/2023.12.17EURUSD(2)_TICK_UTCPlus02-TICK-No Session.csv")
+#visualise the df
+#print(df)
 df['Index'] = df.index
 #create new column with Ask - Bid
 df['Spread'] = df['Ask'] - df['Bid']
-#visualise the df
-#print(df)
 
-
-#Plotting the scatter plot
+#Plotting the scatter plot (& shrink marker size)
 plt.scatter(df.iloc[:, -2], df.iloc[:, -1], label=df.columns[-1] + ' vs ' + df.columns[-2], s = 5)
 
 #Adding a legend
